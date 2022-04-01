0f50766c3054643923e9d3f36468f7f6b1ad9a56
Nico Guba <nguba@mac.com>
2022-04-01 12:05:10 +0100

    context for presentation layer tests

---
+++ src/test/java/io/github/nguba/lunanera/presentation/PresentationLayerTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.nguba.lunanera.presentation;
+
+import io.github.nguba.lunanera.application.LunaNeraApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationLayerTestApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(PresentationLayerTestApplication.class, args);
+    }
+}
